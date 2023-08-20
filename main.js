5f441dae5b75373ee711cd1f00bb00486bfee185
katerina-shche <asti.katerina@gmail.com>
2023-08-20 15:40:25 +0200

    slider listeners

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 const slides = document.querySelectorAll('.slide');
-slides.forEach(i => console.log(i.classList[1]));
+const classesList = [...slides].map(i => i.classList[1]);
+console.log(classesList);
 const rightbtn = document.querySelector('#rightbtn');
 console.log(rightbtn);
 const leftbtn = document.querySelector('#leftbtn');
 console.log(leftbtn);
+
+//functions
+const handleRightClick = () => {
+ console.log('moove to the right')
+ slides.forEach(slide => {
+    console.log(classesList.indexOf(slide.classList[1]))
+ })
+}
+const handleLeftClick = () => {
+    console.log('moove to the left')
+   }
+
+//Event Listeners
+
+rightbtn.addEventListener('click', handleRightClick);
+leftbtn.addEventListener('click', handleLeftClick);

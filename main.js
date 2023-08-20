cf7b1945ba501e67c21b3f7212121f56a84876ba
katerina-shche <asti.katerina@gmail.com>
2023-08-20 15:21:05 +0200

    js set up

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 const slides = document.querySelectorAll('.slide');
-console.log(slides);
+slides.forEach(i => console.log(i.classList[1]));
+const rightbtn = document.querySelector('#rightbtn');
+console.log(rightbtn);
+const leftbtn = document.querySelector('#leftbtn');
+console.log(leftbtn);

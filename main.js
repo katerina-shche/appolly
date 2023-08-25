45ad1e9078ac1a8e175ec5ed3863ac2fe51d66d2
katerina-shche <asti.katerina@gmail.com>
2023-08-25 07:02:07 +0200

    dots

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const slides = document.querySelectorAll('.slide');
-
+const dots = document.querySelectorAll('#dots-container svg');
+console.log(dots);
+const classesListOnLoad = [...slides].map(i => i.classList[1]);
 const rightbtn = document.querySelector('#rightbtn');
 console.log(rightbtn);
 const leftbtn = document.querySelector('#leftbtn');
@@ -8,31 +10,36 @@ console.log(leftbtn);
 //functions
 // slider btns handlers
 const handleLeftClick = () => {
- console.log('moove to the right')
+    // save current styles position on divs
  let classesList = [...slides].map(i => i.classList[1]);
+    // move saved classes 1 div backwards
  slides.forEach((slide, index, slides) => {
     if (index > 0) {
         slide.classList.replace(slide.classList[1], classesList[index - 1])
     } else if (index === 0) {
         slide.classList.replace(slide.classList[1], classesList[classesList.length - 1])
-
-    }
+        }
  })
 }
 const handleRightClick = () => {
     let myClass = slides[0].classList[1];
-    console.log('moove to the left')
     slides.forEach((slide, index, slides) => {
-        console.log(slide, index, slides);
-        
         if ( index < slides.length - 1 ) {
             slide.classList.replace(slide.classList[1], slides[index + 1].classList[1])
         } else if (index == slides.length - 1) { 
             slide.classList.replace(slide.classList[1], myClass)}
      })  
-   }
-    
+}
+const handleDotClick = (index) => {
+    console.log(index)
+   let myClasses = [... classesListOnLoad.slice(index, classesListOnLoad.length), ...classesListOnLoad.slice(0, index)];
+   console.log(myClasses);
+   slides.forEach((slide, slideIndex) => {
+    slide.classList.replace(slide.classList[1], myClasses[slideIndex]);
+   })
+}  
 //Event Listeners
 
 rightbtn.addEventListener('click', handleRightClick);
 leftbtn.addEventListener('click', handleLeftClick);
+dots.forEach((dot, index) => dot.addEventListener('click', () => handleDotClick(index)))
